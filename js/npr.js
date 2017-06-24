3f4494ddfa9516b29e8cc079cf710dffa046a464
DHLAP\h <vdavidhammond@gmail.com>
2017-06-24 13:50:00 -0600

    Got articles to display and created a button that cycles through others.  Need to re create a nicer button and work on the logic to get the articles more defined.

--- js/npr.js
+++ js/npr.js
@@ -1,24 +1,41 @@
+	
+var x = 0;
+var y = 3;
 $(document).ready(function() {
 	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
+	url += '?' + $.param({
   'api-key': "6aaccd1f27414fe993e6fad238be539b",
   'q': "denver, colorado",
   'page': 2
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(response) {
-  var articleURL = response.response.docs[0].web_url;
-  var articleSnippet = response.response.docs[0].snippet;
-  var imageUrl = response.response.docs[0].multimedia[0].url;
-  var articleHeadline = response.response.docs[0].headline.main;
-  console.log(articleURL);
-  console.log(articleSnippet);
-  console.log(imageUrl);
-  console.log(articleHeadline);
-  $("#testArea").append("<div class='col-lg-2'><p>" + articleHeadline + "</p><a href='" + articleURL + "'><img class='img-responsive' alt='Article Image' src='https://static01.nyt.com/" + imageUrl + "'></a><p>" + articleSnippet + "</p>")
+	});
+		$.ajax({
+  		url: url,
+ 		 method: 'GET',
+		}).done(function(response) {
+		function displayArticles () {
+			for (var i = x; i < y; i++) {
+	 		var article = response.response.docs[i];
+	 			if (article.multimedia.length > 0) {
+	 			var articleURL = response.response.docs[i].web_url;
+			  	var articleSnippet = response.response.docs[i].snippet;
+			  	var imageUrl = response.response.docs[i].multimedia[0].url;
+			  	var articleHeadline = response.response.docs[i].headline.main;
+			  	console.log(imageUrl);
+			  	$("#testArea").append("<a href='" + articleURL + "'><div class='col-md-2 col-lg-2 carousel-item active' id='boxBetter'><p class='headLine'>" + articleHeadline + "</p><img class='img-responsive d-block img-fluid' alt='Article Image' src='https://static01.nyt.com/" + imageUrl + "'><p class='snippet'>" + articleSnippet + "</p></div></a>");
+				}
+ 			}			
+};
+		displayArticles()
+		$("#newArticles").click(function () {
+		$("#testArea").empty();
+			x += 3;
+			y += 3;
+			displayArticles();
+
+		});
+  
 }).fail(function(err) {
   throw err;
 });
+  
 });
