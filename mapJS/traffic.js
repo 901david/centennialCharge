71c2c2c75133fe7ccb0768e6acf22e3eb85727ff
dsapkota <dipta.sapkota@viasat.com>
2017-07-02 09:30:41 -0600

    map js

---
+++ mapJS/traffic.js
@@ -0,0 +1,8 @@
+
+
+  width="450"
+  height="250"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&q=record+mountain+in+Colorado" allowfullscreen>
+
+   
