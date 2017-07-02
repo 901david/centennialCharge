0b1642913e4511220e58c52e7e943d2c3ed63350
DHLAP\h <vdavidhammond@gmail.com>
2017-07-02 07:32:44 -0600

    Comment section now updating based on Score.

--- js/comment.js
+++ js/comment.js
@@ -8,8 +8,7 @@ var config = {
   messagingSenderId: "284867902243"
 };
 firebase.initializeApp(config);
-var databaseRef = firebase.database().ref("/" + pageValue + "");
-var databaseRefUser = firebase.database().ref("/" + pageValue + "/" + userName + "/" + userComment);
+var databaseRef = firebase.database().ref(pageValue);
 var userName;
 var userComment;
 $(document).ready(function(){
@@ -19,69 +18,64 @@ $(document).ready(function(){
   userComment = $("#commentInput").val();
   $("#nameInput").val("");
   $("#commentInput").val("");
-  databaseRefUser.set({
+  databaseRef.push({
     name: userName,
     comment: userComment,
     likes: 0,
     dislikes: 0,
     score: 0
   });
-
 });
+
  databaseRef.on("value", function (snapshot) {
-  
-  $("#commentSection").empty();
-  var rootVal = snapshot.val();
-  Object.keys(rootVal).forEach(function(key){
-    
-    Object.keys(rootVal[key]).forEach(function(innerKey)
-    {
-      console.log(rootVal[key][innerKey].name);
-      console.log(rootVal[key][innerKey].comment);
-      console.log(rootVal[key][innerKey].score);
-
-     $("#commentSection").append("<div class='well'><p>" + rootVal[key][innerKey].name + ":                " + rootVal[key][innerKey].comment + "<br/><br/><br/><p>Likes: " + rootVal[key][innerKey].likes + " Dislikes: " + rootVal[key][innerKey].dislikes + "</p><div id='likeButt" + key.split(' ').join('') + innerKey.split(' ').join('') + "' class='btn btn-primary likeButt' data-user='" + rootVal[key][innerKey].name + "' data-comment='" + rootVal[key][innerKey].comment + "'>Like</div><div id='dislikeButt" + key.split(' ').join('') + innerKey.split(' ').join('') + "' class='btn btn-primary dislikeButt' data-user='" + rootVal[key][innerKey].name + "' data-comment='" + rootVal[key][innerKey].comment + "'>Dislike</div></p></div>");
-
-     $("#likeButt" + key.split(' ').join('') + innerKey.split(' ').join('')).click(function () {
-
-      console.log(rootVal[key][innerKey].score);
-      var userCall = $(this).attr("data-user");
-      var userComm = $(this).attr("data-comment");
-      var tempLink = firebase.database().ref("/" + pageValue + "/" + userCall + "/" + userComm);
+    $("#commentSection").empty();
+    console.log(snapshot.val());
+    var sortedCommentsFromValueSnapshot = sortComments(snapshot.val());
       
-      tempLink.once("value", function (snapshot) {
-        var userServerObj = snapshot.val();
-        var userLikes = userServerObj.likes;
-        var userDislikes = userServerObj.dislikes;
-        userLikes += 1;
-        tempLink.update({likes: userLikes});
-        var scoreTot = userLikes - userDislikes;
-        tempLink.update({score: scoreTot});
-
-      });
-    });
-     $("#dislikeButt" + key.split(' ').join('') + innerKey.split(' ').join('')).click(function () {
-
-      console.log(rootVal[key][innerKey].score);
-      var userCall = $(this).attr("data-user");
-      var userComm = $(this).attr("data-comment");
-      var tempLink = firebase.database().ref("/" + pageValue + "/" + userCall + "/" + userComm);
-      
-      tempLink.once("value", function (snapshot) {
-        var userServerObj = snapshot.val();
-        var userLikes = userServerObj.likes;
-        var userDislikes = userServerObj.dislikes;
-        userDislikes += 1;
-        tempLink.update({dislikes: userDislikes});
-        var scoreTot = userLikes - userDislikes;
-        tempLink.update({score: scoreTot});
-      });
-    });
-   });
-
-  }, function (error) {
-
+    sortedCommentsFromValueSnapshot.forEach(child => createCommentSection(child));
   });
 
-});
+ function sortComments(snapshotObject)
+ {
+      var arrayToSort = [];
+
+      Object.keys(snapshotObject).forEach(function(key)
+      {
+        var snapshotChildObject = snapshotObject[key];
+        snapshotChildObject.key = key;
+        arrayToSort.push(snapshotChildObject);
+      });
+
+      arrayToSort.sort(function(childOne, childTwo) {
+        if (childOne.score < childTwo.score) {
+          return 1;
+        }
+        if (childOne.score > childTwo.score) {
+          return -1;
+        }
+        return 0;
+      });
+
+      return arrayToSort;
+ }
+
+ function createCommentSection(snapshot)
+ {
+    $("#commentSection").append("<div class='well'><p>" + snapshot.name + ":                " + snapshot.comment + "<br/><br/><br/><p>Likes: " + snapshot.likes + " Dislikes: " + snapshot.dislikes + "</p><div id='likeButt" + snapshot.key + "' class='btn btn-primary likeButt' data-snapKey='" + snapshot.key + "'>Like</div><div id='dislikeButt" + snapshot.key + "' class='btn btn-primary dislikeButt' data-snapKey='" + snapshot.key + "'>Dislike</div></p></div>");
+
+      $("#likeButt" + snapshot.key).click(function()
+      {
+        var snapkey = snapshot.key;
+        var updateRef = firebase.database().ref(pageValue + "/" + snapkey);
+        var newLikes = snapshot.likes + 1;
+        updateRef.update({likes: newLikes, score: newLikes - snapshot.dislikes});
+      });
+      $("#dislikeButt" + snapshot.key).click(function()
+      {
+        var snapkey = snapshot.key;
+        var updateRef = firebase.database().ref(pageValue + "/" + snapkey);
+        var newDislikes = snapshot.dislikes + 1;
+        updateRef.update({dislikes: newDislikes, score: snapshot.likes - newDislikes});
+      });
+ }
 });
