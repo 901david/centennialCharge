3ed489cc035c22398f693deb1b1fbb1cd26b2601
DHLAP\h <vdavidhammond@gmail.com>
2017-07-03 14:16:20 -0600

    Added Modals in for if you do not fill all fields on the comments entries

--- js/comment.js
+++ js/comment.js
@@ -17,10 +17,14 @@ $(document).ready(function(){
   userName = $("#nameInput").val().trim();
   userComment = $("#commentInput").val().trim();
   if ((userName === "") || (userComment === "")) {
-    // Insert modal here
-    alert("You did not fill outt he fields");
-  }
-  else {
+   $("#notValidInput").removeClass("displayNone");
+   $("#notValidInput").addClass("displayBlock");
+   $("#closeThisNoFields").click(function () {
+    $("#notValidInput").removeClass("displayBlock");
+    $("#notValidInput").addClass("displayNone");
+  });
+ }
+ else {
   $("#nameInput").val("");
   $("#commentInput").val("");
   databaseRef.push({
@@ -34,54 +38,54 @@ $(document).ready(function(){
 });
 
  databaseRef.on("value", function (snapshot) {
-    $("#commentSection").empty();
-    console.log(snapshot.val());
-    var sortedCommentsFromValueSnapshot = sortComments(snapshot.val());
-      
-    sortedCommentsFromValueSnapshot.forEach(child => createCommentSection(child));
-  });
+  $("#commentSection").empty();
+  console.log(snapshot.val());
+  var sortedCommentsFromValueSnapshot = sortComments(snapshot.val());
+  
+  sortedCommentsFromValueSnapshot.forEach(child => createCommentSection(child));
+});
 
  function sortComments(snapshotObject)
  {
-      var arrayToSort = [];
+  var arrayToSort = [];
 
-      Object.keys(snapshotObject).forEach(function(key)
-      {
-        var snapshotChildObject = snapshotObject[key];
-        snapshotChildObject.key = key;
-        arrayToSort.push(snapshotChildObject);
-      });
+  Object.keys(snapshotObject).forEach(function(key)
+  {
+    var snapshotChildObject = snapshotObject[key];
+    snapshotChildObject.key = key;
+    arrayToSort.push(snapshotChildObject);
+  });
 
-      arrayToSort.sort(function(childOne, childTwo) {
-        if (childOne.score < childTwo.score) {
-          return 1;
-        }
-        if (childOne.score > childTwo.score) {
-          return -1;
-        }
-        return 0;
-      });
+  arrayToSort.sort(function(childOne, childTwo) {
+    if (childOne.score < childTwo.score) {
+      return 1;
+    }
+    if (childOne.score > childTwo.score) {
+      return -1;
+    }
+    return 0;
+  });
 
-      return arrayToSort;
- }
+  return arrayToSort;
+}
 
- function createCommentSection(snapshot)
- {
-    $("#commentSection").append("<div class='well col-lg-12 flipInX animated'><p><strong>" + snapshot.name + "</strong><em> has this to say about this page:</em></p><br><p>" + snapshot.comment + "</p><br/><br/><br/><div id='likeButt" + snapshot.key + "' class='btn btn-success likeButt' data-snapKey='" + snapshot.key + "'>Like</div>" + "   " + "<span class='someSpace'>" + snapshot.likes + "</span><div id='dislikeButt" + snapshot.key + "' class='btn btn-danger dislikeButt' data-snapKey='" + snapshot.key + "'>Dislike</div>" + "   " +  "<span class='someSpace'>" + snapshot.dislikes + "</span></div>");
+function createCommentSection(snapshot)
+{
+  $("#commentSection").append("<div class='well col-lg-12 flipInX animated'><p><strong>" + snapshot.name + "</strong><em> has this to say about this page:</em></p><br><p>" + snapshot.comment + "</p><br/><br/><br/><div id='likeButt" + snapshot.key + "' class='btn btn-success likeButt' data-snapKey='" + snapshot.key + "'>Like</div>" + "   " + "<span class='someSpace'>" + snapshot.likes + "</span><div id='dislikeButt" + snapshot.key + "' class='btn btn-danger dislikeButt' data-snapKey='" + snapshot.key + "'>Dislike</div>" + "   " +  "<span class='someSpace'>" + snapshot.dislikes + "</span></div>");
 
-      $("#likeButt" + snapshot.key).click(function()
-      {
-        var snapkey = snapshot.key;
-        var updateRef = firebase.database().ref(pageValue + "/" + snapkey);
-        var newLikes = snapshot.likes + 1;
-        updateRef.update({likes: newLikes, score: newLikes - snapshot.dislikes});
-      });
-      $("#dislikeButt" + snapshot.key).click(function()
-      {
-        var snapkey = snapshot.key;
-        var updateRef = firebase.database().ref(pageValue + "/" + snapkey);
-        var newDislikes = snapshot.dislikes + 1;
-        updateRef.update({dislikes: newDislikes, score: snapshot.likes - newDislikes});
-      });
- }
+  $("#likeButt" + snapshot.key).click(function()
+  {
+    var snapkey = snapshot.key;
+    var updateRef = firebase.database().ref(pageValue + "/" + snapkey);
+    var newLikes = snapshot.likes + 1;
+    updateRef.update({likes: newLikes, score: newLikes - snapshot.dislikes});
+  });
+  $("#dislikeButt" + snapshot.key).click(function()
+  {
+    var snapkey = snapshot.key;
+    var updateRef = firebase.database().ref(pageValue + "/" + snapkey);
+    var newDislikes = snapshot.dislikes + 1;
+    updateRef.update({dislikes: newDislikes, score: snapshot.likes - newDislikes});
+  });
+}
 });
