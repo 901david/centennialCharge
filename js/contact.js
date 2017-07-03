0f4d65494e83e5e045d933a6d701057af046e01d
DHLAP\h <vdavidhammond@gmail.com>
2017-07-03 14:03:00 -0600

    Created modals for the three situations on the contact page when an alert was present.

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,12 @@
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDsYb-UNzHdNFHVtXy5htkfh4Y4mWAJv4M",
-  authDomain: "centennialcharge.firebaseapp.com",
-  databaseURL: "https://centennialcharge.firebaseio.com",
-  projectId: "centennialcharge",
-  storageBucket: "centennialcharge.appspot.com",
-  messagingSenderId: "284867902243"
+	apiKey: "AIzaSyDsYb-UNzHdNFHVtXy5htkfh4Y4mWAJv4M",
+	authDomain: "centennialcharge.firebaseapp.com",
+	databaseURL: "https://centennialcharge.firebaseio.com",
+	projectId: "centennialcharge",
+	storageBucket: "centennialcharge.appspot.com",
+	messagingSenderId: "284867902243"
 };
 firebase.initializeApp(config);
 var databaseRefContact = firebase.database().ref("/contactUs");
@@ -18,28 +18,49 @@ $(document).ready(function(){
 		var emailVal = $("#email").val().trim();
 		var messageVal = $("#message").val().trim();
 		if ((nameVal === "") || (emailVal === "") || (messageVal === "")) {
-			// insert modal here
-			alert("you did not fill out all the fields");
+			$("#notAllFields").removeClass("displayNone");
+			$("#notAllFields").addClass("displayBlock");
+			$("#closeThisNotFill").click(function () {
+				$("#notAllFields").removeClass("displayBlock");
+				$("#notAllFields").addClass("displayNone");
+			});
+			
 		}
 		else if ((emailVal.includes("@")) && (emailVal.includes("."))){
-		$("#name").val("");
-		$("#email").val("");
-		$("#message").val("");
-		
+			$("#name").val("");
+			$("#email").val("");
+			$("#message").val("");
+			
 			databaseRefContact.push({
-			name: nameVal,
-			email: emailVal,
-			message: messageVal
-		});
-		alert("We received your message");
+				name: nameVal,
+				email: emailVal,
+				message: messageVal
+			});
+			$("#showOverOrNot").removeClass("displayNone");
+			$("#showOverOrNot").addClass("displayBlock");
+			$("#closeThisThanks").click(function () {
+				$("#showOverOrNot").removeClass("displayBlock");
+				$("#showOverOrNot").addClass("displayNone");
+			});
 		}
 		else {
-			// insert modal here
-			alert("You did not insert a valid email.");
+			$("#notValid").removeClass("displayNone");
+			$("#notValid").addClass("displayBlock");
+			$("#closeThisNoEmail").click(function () {
+				$("#notValid").removeClass("displayBlock");
+				$("#notValid").addClass("displayNone");
+			});
 		}
 		
 		
 		
 		
 	});
-	});
+
+
+	
+
+
+	
+	
+});
