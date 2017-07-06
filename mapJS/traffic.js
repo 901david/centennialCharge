635fb760a75aac7e1992cb3fecd178841a9e3050
dsapkota <dipta.sapkota@viasat.com>
2017-07-05 19:29:31 -0600

    updated traffic.js

--- mapJS/traffic.js
+++ mapJS/traffic.js
@@ -1,9 +1,9 @@
 
 
  function initMap() {
-        var map = new google.maps.Map(document.getElementById("map"), {
+        var map = new google.maps.Map(document.getElementById("mapBox"), {
           zoom: 13,
-          center: {lat: 34.04924594193164, lng: -118.24104309082031}
+          center: {lat: 39.742043, lng: -104.991531}
         });
 
         var trafficLayer = new google.maps.TrafficLayer();
