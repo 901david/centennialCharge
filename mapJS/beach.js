5254135655eddb354fe2c98065bcd04cd6147406
Sapkota <dsapkota@hq.corp.viasat.com>
2017-07-06 10:27:51 -0600

    added marker to the map

--- mapJS/beach.js
+++ mapJS/beach.js
@@ -1,55 +1,53 @@
 
 function initMap() {
-        var map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 10,
-          center: {lat: -33.9844444444, lng: 118.471666667}
+
+   var jerseyShore = {
+     info: "Jersey Shore is a Beach in Lycoming County, Pennsylvania. It has an elevation of 189 meters, or 620 feet",
+    lat: 41.202017,
+    long: -77.264415
+   };
+ 
+   var veniceBeach  = {
+     info: "Venice Beach is a Beach in Los Angeles County, California. It has an elevation of 1 meters, or 3 feet.",
+     lat: 33.9844444444,
+     long: -118.471666667
+   };
+ 
+   var sunsetBeach = {
+     info: "Sunset Beach Treasure Island, FL, USA",
+     lat: 27.745003,
+     long: -82.759659
+   };
+ 
+   var beaches = [
+       [jerseyShore .info, jerseyShore .lat, jerseyShore .long, 0],
+       [veniceBeach.info, veniceBeach.lat, veniceBeach.long, 1],
+       [sunsetBeach.info, sunsetBeach.lat, sunsetBeach.long, 2],
+     ];
+ 
+   var map = new google.maps.Map(document.getElementById("map"), {
+     zoom: 3,
+     center: new google.maps.LatLng(36.778259, -119.417931),
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+   });
+ 
+   var marker, i;
+ 
+   for (i = 0; i < beaches.length; i++) {
+    console.log(beaches[i]);
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(beaches[i][1], beaches[i][2]),
+      map: map
+    });
+
+    (function(marker, i) {
+        // add click event
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow = new google.maps.InfoWindow({
+                content: beaches[i][0]
+            });
+            infowindow.open(map, marker);
         });
-
-        setMarkers(map);
-      }
-
-      // Data for the markers consisting of a name, a LatLng and a zIndex for the
-      // order in which these markers should display on top of each other.
-      var beaches = [
-        ['Venice Beach', -118.471666667, 33.9844444444, 3],
-        ['Jersey Shore', -77.264415, 41.202017, 2],
-        ['Sunset Beach', -33.8551255, 18.4914073, 1],
-        
-      ];
-
-      function setMarkers(map) {
-        // Adds markers to the map.
-
-        // Marker sizes are expressed as a Size of X,Y where the origin of the image
-        // (0,0) is located in the top left of the image.
-
-        // Origins, anchor positions and coordinates of the marker increase in the X
-        // direction to the right and in the Y direction down.
-        var image = {
-          url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          // This marker is 20 pixels wide by 32 pixels high.
-          size: new google.maps.Size(20, 32),
-          // The origin for this image is (0, 0).
-          origin: new google.maps.Point(0, 0),
-          // The anchor for this image is the base of the flagpole at (0, 32).
-          anchor: new google.maps.Point(0, 32)
-        };
-        // Shapes define the clickable region of the icon. The type defines an HTML
-        // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-        // The final coordinate closes the poly by connecting to the first coordinate.
-        var shape = {
-          coords: [1, 1, 1, 20, 18, 20, 18, 1],
-          type: 'poly'
-        };
-        for (var i = 0; i < beaches.length; i++) {
-          var beach = beaches[i];
-          var marker = new google.maps.Marker({
-            position: {lat: beach[1], lng: beach[2]},
-            map: map,
-            icon: image,
-            shape: shape,
-            title: beach[0],
-            zIndex: beach[3]
-          });
-        }
-      }
+    })(marker, i);
+   }
+ }
